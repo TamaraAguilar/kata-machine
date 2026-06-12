eae6788d60f626745b1da977cef3659b8281207f
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-06-11 22:37:08 -0400

    feat: implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -3,16 +3,16 @@ export default function bs_list(haystack: number[], needle: number): boolean {
     let upper_bound: number = haystack.length;
 
     while (lower_bound < upper_bound) {
-        let middle: number = Math.floor(
+        const middle: number = Math.floor(
             lower_bound + (upper_bound - lower_bound) / 2,
         );
 
-        let value = haystack[middle];
+        const value = haystack[middle];
 
         if (value === needle) return true;
-        if (value > needle) {
+        else if (value > needle) {
             upper_bound = middle;
-        } else if (value < needle) {
+        } else {
             lower_bound = middle + 1;
         }
     }
